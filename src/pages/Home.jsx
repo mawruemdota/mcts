3638de4f080f04c1c2dd2c0e7aff345b16bc6eb5
Marasigan Creative Tech Solutions <marasigancts@gmail.com>
2025-11-11 00:41:43 +0000

    Manual code change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -239,7 +239,7 @@ export default function HomePage() {
 
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
           <div className="text-center max-w-3xl mx-auto">
-            <h2 className="text-white mb-6 text-4xl font-black lowercase sm:text-5xl lg:text-6xl">Bring Your Ideas to Impact</h2>
+            <h2 className="text-white mb-6 text-4xl font-black lowercase sm:text-5xl lg:text-6xl">your ideas to impact</h2>
             <p className="text-xl text-white/90 mb-8">
               From creative design to printable outputs, kami ang bahala sa inyo!
             </p>
