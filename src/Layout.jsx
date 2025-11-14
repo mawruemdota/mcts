5dfc6e13752f72c68c2fad2921564457ba2ac020
Marasigan Creative Tech Solutions <marasigancts@gmail.com>
2025-11-14 06:10:26 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useCallback } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -1,4 +1,3 @@
-import { useToast } from "@/components/ui/use-toast";
 import {
   Toast,
   ToastClose,
@@ -6,16 +5,17 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast";
+} from "@/components/ui/toast"
+import { useToast } from "@/components/ui/use-toast"
 
 export function Toaster() {
-  const { toasts } = useToast();
+  const { toasts } = useToast()
 
   return (
-    <ToastProvider>
+    <ToastProvider duration={3000}>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast key={id} duration={3000} {...props}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
@@ -25,9 +25,9 @@ export function Toaster() {
             {action}
             <ToastClose />
           </Toast>
-        );
+        )
       })}
       <ToastViewport />
     </ToastProvider>
-  );
-} 
+  )
+}
